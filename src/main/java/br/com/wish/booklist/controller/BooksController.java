03237f3070069e6ba7978cb7637a537382669e29
devGeysaLadica <geysa.ladica@gmail.com>
2021-11-28 17:36:03 -0300

    creat bookRepository interface and inject dependency into the BookController

--- src/main/java/br/com/wish/booklist/controller/BooksController.java
+++ src/main/java/br/com/wish/booklist/controller/BooksController.java
@@ -2,16 +2,24 @@ package br.com.wish.booklist.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wish.booklist.controller.dto.BookDto;
+import br.com.wish.booklist.modelo.Book;
+import br.com.wish.booklist.repository.BookRepository;
 
 @RestController
 @RequestMapping("/books")
 public class BooksController {
 	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	
 	public List<BookDto> books(String nameCategory){
-		
+		List<Book> books = bookRepository.findAll();
+		return BookDto.convert(books);
 	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO CATEGORYBOOK (title) VALUES ('Action');
+INSERT INTO CATEGORYBOOK (title) VALUES ('Drama');
+INSERT INTO CATEGORYBOOK (title) VALUES ('Romance');
+
+INSERT INTO BOOK (title, author, status, category_id) 
+VALUES ('Termination Shoc', 'Neal Stephenson', 'READ', 1 );
+
+INSERT INTO BOOK(title, author, status, category_id) 
+VALUES('A Raisin in the Sun ', 'Lorraine Hansberry', 'READ', 2 );
+
+INSERT INTO BOOK(title, author, status, category_id) 
+VALUES('The Time Machine', 'H.G Wells', 'READ', 3 );
+
+
+
